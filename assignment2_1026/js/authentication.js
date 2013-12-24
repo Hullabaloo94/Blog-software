0cc76025444243fd8175be597806883638fb5acb
Hullabaloo94 <jasonahall1994@gmail.com>
2013-12-24 14:26:03 +0000

    removed as this file is not used or functional

--- assignment2_1026/js/authentication.js
+++
@@ -1,9 +0,0 @@
-var dataRef = new Firebase("https://blog-store.firebaseio.com/");
-// Log me in.
-dataRef.auth(AUTH_TOKEN, function(error) {
-  if(error) {
-    console.log("Login Failed!", error);
-  } else {
-    console.log("Login Succeeded!");
-  }
-});
